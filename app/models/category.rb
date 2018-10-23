d554525cb9dea54c02e07307dfa534f7865d1655
github username <github email address>
2018-10-23 11:37:56 -0500

    Implement the category dependency associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
-    has_many :dependent_categories, class_name: "Dependency", foreign_key: :dependee_id 
+    has_many :dependent_categories, class_name: "Dependency", foreign_key: :dependee_id
     has_many :dependents, through: :dependent_categories, source: :dependent
+
+    has_many :dependency_categories, class_name: "Dependency", foreign_key: :dependent_id
+    has_many :dependencies, through: :dependency_categories, source: :dependee
 
 end
--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -1,2 +1,8 @@
 class Dependency < ApplicationRecord
+    belongs_to :dependent, foreign_key: :dependent_id, class_name: "Category"
+    belongs_to :dependee, foreign_key: :dependee_id, class_name: "Category"
+    # def crazy
+    #     return "Crazy"
+    # end
+
 end
