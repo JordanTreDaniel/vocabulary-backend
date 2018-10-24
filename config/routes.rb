448c8beca838e39ff15355043128f023cb5f883b
github username <github email address>
2018-10-24 17:33:20 -0500

    Build controllers: card, category, tag + Corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+     resources :cards, only: [:index, :show]
+     resources :categories, only: [:index, :show]
+     resources :tags, only: [:index, :show]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: "application#test"
 end
