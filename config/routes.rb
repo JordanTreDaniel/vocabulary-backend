3c2eea70f37584b051f078eaec1bffba53129d99
github username <github email address>
2018-11-07 17:13:46 -0600

    Add route for updating categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
      resources :cards, only: [:index, :show]
-     resources :categories, only: [:index, :show]
+     resources :categories, only: [:index, :show, :update]
      resources :tags, only: [:index, :show]
     end
   end
