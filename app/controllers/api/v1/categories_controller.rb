73bb9aed5f092ecb2a4cc2ee0a8f25cd58bd55db
github username <github email address>
2018-11-14 10:14:26 -0600

    Change category to not use serializer

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,11 +5,13 @@ class Api::V1::CategoriesController < ApplicationController
     end
     def show
         category = Category.find(params[:id])
-        render :json => category, include: ['cards']
+        render :json => category, methods: [:some_arbitrary_method]
     end
     def update
-        category = Category.find(params[:id])
-        byebug
+        @category = Category.find(params[:id])
         render :json => category
+    end
+    def category_params
+        params[:data].permit(:id, :type, :attributes => [:name, "img-url", :desc], :relationships => [:cards => [:data], :dependencies => [:data], :dependents => [:data]]).to_h
     end
 end
