6e1b7b79a9a7e6345e252811bf44ca585e4b01ed
github username <github email address>
2018-11-15 14:08:35 -0600

    Modify the CategoriesController to update attached cards

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -8,14 +8,23 @@ class Api::V1::CategoriesController < ApplicationController
         render :json => category, methods: [:cards]
     end
     def update
+        @cards = []
+        params[:cards].each do |card|
+            @card = Card.find(card[:id])
+            @cards.push(@card.update!(card_params(card)))
+        end
         @category = Category.find(params[:id])
+        byebug
         if @category.update(category_params)
-            render :json => @category
+            render :json => @category, methods: [:cards]
         else
             render :json => @category.errors
         end
     end
     def category_params
-        params.require(:category).permit(:id, :desc, :img_url, :name,  :created_at, :updated_at)
+        params.require(:category).permit(:id, :desc, :img_url, :name, :cards, :created_at, :updated_at)
+    end
+    def card_params(cardObj)
+        cardObj.permit(:id, :term, :desc, :def, :created_at, :updated_at)
     end
 end
